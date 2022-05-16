5023c15b901dc321d6f41cb584ed79e356648ff8
root <root@ip-172-31-13-97.ap-south-1.compute.internal>
2022-05-16 07:04:29 +0000

    conditional program files added

---
+++ switch.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "enter the number"
+read n
+case $n in
+	1) echo "one" ;;
+	2) echo "two" ;;
+	3) echo "three" ;;
+	*) echo "wrong number";;
+esac
+
---
+++ vowel.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "enter alphabet"
+read n
+case $n in
+	a) echo "vowel" ;;
+	e) echo "vowel" ;;
+	i) echo "vowel" ;;
+	o) echo "vowel" ;;
+	u) echo "vowel" ;;
+	*) echo "consonent" ;;
+esac
